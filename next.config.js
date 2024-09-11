2667984cad29f2f3aac52d6b31a44148defe6c4d
Staslios <>
2024-09-11 16:37:46 +0200

    test

--- next.config.js
+++ next.config.js
@@ -11,13 +11,16 @@ const nextConfig = {
 
   webpack(config, { isServer, webpack }) {
 
+    const PROVIDER_ONE_URL = process.env.PROVIDER_ONE_URL ?? 'http://localhost:3001'
+    const PROVIDER_TWO_URL = process.env.PROVIDER_ONE_URL ?? 'http://localhost:3002'
+
     config.plugins.push(
       new NextFederationPlugin({
         name: 'host',
         filename: 'static/chunks/remoteEntry.js',
         remotes: {
-          'provider-one': `provider-one@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
-          'provider-two': `provider-two@http://localhost:3002/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+          'provider-one': `provider-one@${PROVIDER_ONE_URL}/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+          'provider-two': `provider-two@${PROVIDER_TWO_URL}/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
         },
         extraOptions:{
           debug: process.env.NODE_ENV !== 'production',
