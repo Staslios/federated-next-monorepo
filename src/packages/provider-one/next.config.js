ef6ff8e09fee1f8e8f60f9d252d6a2255afd5652
Staslios <>
2024-09-13 18:18:52 +0200

    added next debug mode

--- src/packages/provider-one/next.config.js
+++ src/packages/provider-one/next.config.js
@@ -11,8 +11,6 @@ const nextConfig = {
 
   webpack(config, { isServer, webpack }) {
 
-    const PROVIDER_TWO_URL = process.env.PROVIDER_TWO_URL ?? 'http://localhost:3002'
-
     config.plugins.push(
       new NextFederationPlugin({
         name: 'provider-one',
@@ -20,9 +18,6 @@ const nextConfig = {
         exposes: {
           './ExternalA': './src/components/ExternalizedA/ExternalizedA.js',
           './CatFact': './src/components/CatFact/CatFact.js'
-        },
-        remotes: {
-          'provider-two': `provider-two@${PROVIDER_TWO_URL}/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
         },
         extraOptions: {
           debug: process.env.NODE_ENV !== 'production',
